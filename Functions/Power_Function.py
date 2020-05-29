bc8a3996036348a4c1ca490fdde1dc1320b0f38d
am-isse <amina.isse@outlook.com>
2020-05-28 23:34:45 -0400

    Moved test function to separate file

--- Functions/Power_Function.py
+++ Functions/Power_Function.py
@@ -1,5 +1,5 @@
 #Goal: Calculate the specified power of a specified base
-#MHandling: Does not compute complex numbers 
+#Handling: Does not compute complex numbers 
 
 from math import floor
 from math import sqrt
@@ -80,42 +80,3 @@ def power_fractional (base, power):
         return -0.0
     
     return decimal_result * integer_result
-
-def text_function ():
-    answer = power_function(0,0.1)
-    if (str(answer) != "-0.0"):
-        print(answer)
-        #Output-> 0
-        #Validity-> Correct 
-    answer = power_function(0,-0.1)
-    if (str(answer) != "-0.0"):
-        print(answer)
-        #Output-> ERROR: float division by zero
-        #Validity-> Correct: Answer is a complex number
-    answer = power_function(2,1.1)
-    if (str(answer) != "-0.0"):
-        print(answer)
-        #Output-> 2.143546925072451
-        #Validity-> Correct
-    answer = power_function(2,2)
-    if (str(answer) != "-0.0"):
-        print(answer)
-        #Output-> 4
-        #Validity-> Correct
-    answer = power_function(4,sqrt(2))
-    if (str(answer) != "-0.0"):
-        print(answer)
-        #Output-> 7.1029933013161255
-        #Validity-> Correct
-    answer = power_function(4,4)
-    if (str(answer) != "-0.0"):
-        print(answer)
-        #Output-> 256
-        #Validity-> Correct
-    answer = power_function(-0.5,-0.5)
-    if (str(answer) != "-0.0"):
-        print(answer)
-        #Output-> ERROR: math domain error
-        #Validity-> Correct: Answer is a complex number
-
-text_function()
