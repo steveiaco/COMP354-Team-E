4348acc10bf9fd214551f6ff7590c78e48441196
jinchen-hu <jinchenhu.luke@gmail.com>
2020-05-29 18:10:37 -0400

    Use our own power functions

--- Functions/Standard_deviation.py
+++ Functions/Standard_deviation.py
@@ -1,37 +1,36 @@
 import numpy as np
-import time
-
-arr = np.array([1,2,3,4,5,6,7,8,9,10])
-print("Computed by numpy: " + str(np.std(arr)))
-
-sta = end = time.time() * 10E10
-def std_noUseNumpy(arr):
-    if len(arr)==0 or len(arr)==1:
-        return 0;
-    x_bar, s = sum(arr)/len(arr), 0
-    arr_new = (arr - x_bar) ** 2
-    return (sum(arr_new) / len(arr)) ** 0.5
+import Power_Function as pf
 
 
-end = time.time() * 10E10
-print("Computer by original python: " + str(std_noUseNumpy(arr)) + ". Time taken: " + str(end-sta))
+def standard_deviation(input):
+    count = len(input)
+    if count > 0:
+        # if the length of argument list is 1, return 0 directly
+        if count == 1:
+            return 0;
+        # compute mean
+        args = np.array(input)
+        mean, s = total_count(args)/count, 0
+        # get a new list after each item minus mean, and square
+        args_new = args - mean
+        for i in range(count):
+            args_new[i] = pf.power_integer(args_new[i], 2)
 
-sta = end = time.time() * 10E10
-def std_forLoop(arr):
-    l = len(arr)
-    if l==0 or l==1:
-        return 0
-    s = arr[0]
-    for i in range(1, l):
-        s += arr[i]
-    avg = s/l
-    s = (arr[0] - avg) ** 2
-    for i in range(1, l):
-        s += (arr[i] - avg) ** 2
-    return (s/l) ** 0.5
+        return pf.power_fractional(total_count(args_new) / count, 0.5)
+    else:
+        raise TypeError('Invalid Argument !')
 
 
-end = time.time() * 10E10
-print("Computed by for loop:        " + str(std_forLoop(arr)) + ". Time taken: " + str(end-sta))
+def total_count(args):
+    s = 0
+    for i in range(len(args)):
+        s = s + args[i]
+    return s
+
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+print("Computed by numpy:           " + str(np.std(arr)))
+print("Computed by original python: " + str(standard_deviation(arr)))
+
     
 
