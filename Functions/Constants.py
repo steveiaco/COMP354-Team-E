e47b8138423e85a6df5fe0d0571f8d812bda5d56
steveiaco <31911579+steveiaco@users.noreply.github.com>
2020-05-30 22:04:04 -0400

    Added Constants.py along with initial calculation algorithms for e and pi

---
+++ Functions/Constants.py
@@ -0,0 +1,43 @@
+from Power_Function import power_function
+import math
+
+# Initial values
+pi = 0
+e = 0
+
+# Calculate pi using the Chudnovsky algorithm
+def get_pi():
+    global pi
+
+    if pi == 0:
+        series_sum = 0
+
+        # We iterate for 20 iterations, as Chudnosvky converges very quickly
+        for i in range(20):
+            series_sum += (power_function([-1, i]) * math.factorial(6 * i) * (13591409 + (545140134 * i)) ) / (math.factorial(3 * i) * power_function([math.factorial(i),3]) * power_function([640320, 3*i + 1.5]))
+
+        pi = 1 / (12 * series_sum)
+
+    return pi
+
+def get_e():
+    global e
+
+    if e == 0:
+        series_sum = 0
+
+        for i in range(1000):
+            series_sum += (1 / math.factorial(i))
+
+    return e
+
+def main():
+
+    print(get_pi())
+    print(get_pi())
+    print(get_e())
+    print(get_e())
+
+
+if __name__ == "__main__":
+    main()
