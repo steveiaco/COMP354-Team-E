6997c12de87dbb15effd52aa34ef1bf38b564df9
Wayne Huras <wayne.stamand@hotmail.com>
2020-05-30 21:47:37 -0400

    added cosh function, including factorial function

---
+++ Functions/Cosh.py
@@ -0,0 +1,27 @@
+# Done - Exponential: cosh(x) = (e^x + e^(-x))/2
+# Taylor: cosh(x) = 1 + (x^2/2!) + (x^4/4!) + (x^6/6!) ...
+
+from Power_Function import power_function
+
+
+def cosh(args):
+    e = 2.718281828459045 # replace from constant file once created
+    x = 0
+    if(len(args) == 1):
+        x = args[0]
+    else:
+        raise Exception(f"Invalid number of arguments, power_function got {len(args)} but expected 1.")
+    return (power_function([e, x]) + power_function([e, -x])) / 2
+
+
+# Helper functions
+def factorial(input):
+    temp = input
+    while temp > 1:
+        temp -= 1
+        input *= temp
+    return input
+
+
+# Test
+print(cosh([5])) # 74.2099485248
