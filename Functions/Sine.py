f900392a6d96ed70cf68e53d1a7c2343ba0c8494
yongsuH <65259683+yongsuH@users.noreply.github.com>
2020-06-01 10:58:11 -0400

    Improved logic
    Now fast and accurate when input is very large, accurate to 15 decimal places when input = 10^9

--- Functions/Sine.py
+++ Functions/Sine.py
@@ -22,13 +22,8 @@ def sine(num):
     pi = 3.141592653589793
     twopi = 2 * pi
 
-    # program will crush if num too big
-    # so minus 2pi continuously, since sin(x+2pi)=sin(x)
-    # this step is losing precision and very slow when num is big, like x=10^9
-    while num > twopi:
-        num -= twopi
-    while num < -twopi:
-        num += twopi
+    # program will crush if num too big, convert to interval 0 -> 2pi
+    num = num % twopi
     # using for loop to create summation i=0 to i=39
     for i in range(40):
         res += (Pwr.power_function([-1, i])) * (Pwr.power_function([num, (2*i+1)])) / (factorial(2*i+1))
