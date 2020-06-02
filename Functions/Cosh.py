547007ad42dd79559ff9115c70d91e4c12d2d643
Wayne Huras <wayne.stamand@hotmail.com>
2020-06-02 19:24:44 -0400

    re-added function arguments to be a list rather than a number.

--- Functions/Cosh.py
+++ Functions/Cosh.py
@@ -1,12 +1,16 @@
-# Done - Exponential: cosh(x) = (e^x + e^(-x))/2
-# Taylor: cosh(x) = 1 + (x^2/2!) + (x^4/4!) + (x^6/6!) ...
-
 from Power_Function import power_function
 from Constants import get_e()
 
-
-def cosh(number):
-    x = number
+# Goal: calculate cosh(x)
+# Method: exponential, cosh(x) = (e^x + e^(-x))/2
+def cosh(args):
+    x = 0
     e = get_e()
+
+    # if the correct number of arguments are passed, then continue
+    if len(args) == 1:
+        x == args[0]
+    else:
+        raise Exception(f"Invalid number of arguments, power_function got {len(args)} but expected 1.")
 
     return (power_function([e, x]) + power_function([e, -x])) / 2
