a1c236880ce1755b6b68e7aff6d7eb4c1c0330a7
yongsuH <65259683+yongsuH@users.noreply.github.com>
2020-06-02 19:58:33 -0400

    SIne now takes a list with 1 element
    Exception will be raise if length of list != 1

--- Functions/Sine.py
+++ Functions/Sine.py
@@ -18,13 +18,18 @@ def factorial(number):
         return number*factorial(number-1)
 
 
-def sine(num):
+def sine(args):
+    if len(args) == 1:
+        num = args[0]
+    else:
+        raise Exception("Invalid number of input, sine take 1 input which is the number")
     res = 0
     pi = get_pi()
     twopi = 2 * pi
 
     # program will crush if num too big, convert to interval 0 -> 2pi
     num = num % twopi
+
     # using for loop to create summation i=0 to i=79
     for i in range(80):
         res += (Pwr.power_function([-1, i])) * (Pwr.power_function([num, (2*i+1)])) / (factorial(2*i+1))
